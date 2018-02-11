eed491af4fc134aedbcafde7e9216c1d58a15759
Josef Schugt <penpendede@mail.ru>
2018-02-11 18:01:57 +0100

    schemata from json-schema-checker moved to individual JSON files

--- tools/json-schema-checker.js
+++ tools/json-schema-checker.js
@@ -19,106 +19,15 @@ parser.addArgument(
 
 let args = parser.parseArgs()
 
-let pointSchema = {
-  'id': '/point',
-  'type': 'array',
-  'items': { 'type': 'number' },
-  'minItems': 2,
-  'maxItems': 2
-}
-
-let latLngBoundsSchema = {
-  'id': '/latLngBounds',
-  'type': 'array',
-  'items': { 'type': {'$ref': '/point'} },
-  'minItems': 2,
-  'maxItems': 2
-}
-
-let subDomainSchema = {
-  'id': '/subDomain',
-  'oneOf': [
-    { 'type': 'string' },
-    {
-      'type': 'array',
-      'items': {
-        'type': 'string'
-      },
-      'minItems': 1
-    }
-  ]
-}
-
-let layerOptsSchema = {
-  'id': '/layerOpts',
-  'type': 'object',
-  'properties': {
-    'attribution': { 'type': 'string' },
-    'bounds': { 'type': { '$ref': '/latLngBounds' } },
-    'maxZoom': { 'type': 'number' },
-    'minZoom': { 'type': 'number' },
-    'opacity': { 'type': 'number' }
-  }
-}
-
-let layerSourceSchema = {
-  'id': '/layerSource',
-  'type': 'object',
-  'properties': {
-    'uriTemplate': { 'type': 'string' },
-    'opts': { 'type': { '$ref': '/layerOpts' } }
-  },
-  'required': ['uriTemplate']
-}
-
-let layerSchema = {
-  'id': '/layer',
-  'type': 'object',
-  'properties': {
-    'label': { 'type': 'string' },
-    'id': { 'type': 'string' },
-    'about': { 'type': 'string' },
-    'type': { 'type': 'string' },
-    'source': { 'type': { '$ref': '/layerSource' } }
-  },
-  'required': ['label', 'id', 'type', 'source']
-}
-
-let layerFileSchema = {
-  'id': '/layerFile',
-  'type': 'array',
-  'items': { 'type': { '$ref': '/layer' } }
-}
-
-let viewSchema = {
-  'id': '/view',
-  'type': 'object',
-  'properties': {
-    'center': { 'type': { '$ref': '/point' } },
-    'zoom': { 'type': 'number' }
-  }
-}
-
-let startupConfigSchema = {
-  'id': '/startupConfig',
-  'type': 'object',
-  'properties': {
-    'domElementId': { 'type': 'string' },
-    'view': { 'type': { '$ref': '/view' } },
-    'placeHolderBaseLayer': { 'type': { '$ref': '/layer' } }
-  },
-  'required': [ 'domElementId' ]
-}
-
-validator.addSchema(pointSchema, '/point')
-validator.addSchema(latLngBoundsSchema, '/latLngBounds')
-validator.addSchema(subDomainSchema, '/subDomain')
-validator.addSchema(layerOptsSchema, '/layerOpts')
-validator.addSchema(layerSourceSchema, '/layerSource')
-validator.addSchema(layerSchema, '/layer')
-validator.addSchema(layerFileSchema, '/layerFile')
-validator.addSchema(viewSchema, '/view')
-validator.addSchema(startupConfigSchema, '/startupConfig')
+validator.addSchema(JSON.parse(fs.readFileSync('./schemata/pointSchema.json', 'utf8')), '/point')
+validator.addSchema(JSON.parse(fs.readFileSync('./schemata/latLngBoundsSchema.json', 'utf8')), '/latLngBounds')
+validator.addSchema(JSON.parse(fs.readFileSync('./schemata/subDomainSchema.json', 'utf8')), '/subDomain')
+validator.addSchema(JSON.parse(fs.readFileSync('./schemata/layerOptsSchema.json', 'utf8')), '/layerOpts')
+validator.addSchema(JSON.parse(fs.readFileSync('./schemata/layerSourceSchema.json', 'utf8')), '/layerSource')
+validator.addSchema(JSON.parse(fs.readFileSync('./schemata/layerSchema.json', 'utf8')), '/layer')
+validator.addSchema(JSON.parse(fs.readFileSync('./schemata/layerFileSchema.json', 'utf8')), '/layerFile')
+validator.addSchema(JSON.parse(fs.readFileSync('./schemata/viewSchema.json', 'utf8')), '/view')
+validator.addSchema(JSON.parse(fs.readFileSync('./schemata/startupConfigSchema.json', 'utf8')), '/startupConfig')
 
 let abortIfValidationFails = (fileName, verbose, validationResult) => {
   if (!validationResult.valid) {
